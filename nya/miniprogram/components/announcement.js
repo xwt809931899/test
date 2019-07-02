56b108689700c7423b24c91a8d9521362ce561bd
xwt809931899 <809931899@qq.com>
2019-07-02 16:46:49 +0800

    call的实现原理，浅拷贝和深拷贝,闭包的定义和作用

---
+++ nya/miniprogram/components/announcement.js
@@ -0,0 +1,25 @@
+// components/announcement.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    todisappear: false,
+    addText: '这个人很懒什么都没写',
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
